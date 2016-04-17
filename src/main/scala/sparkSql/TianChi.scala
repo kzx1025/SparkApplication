e052bf88b116857255779a28b5ee2d7bdeb2c3cd
iceke <iceke1025@gmail.com>
2016-04-17 20:08:29 +0800

    improve arguments

--- src/main/scala/sparkSql/TianChi.scala
+++ src/main/scala/sparkSql/TianChi.scala
@@ -1,5 +1,8 @@
 package sparkSql
 
+import java.io.PrintWriter
+
+import java.io._
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 
@@ -11,7 +14,7 @@ object TianChi {
 
   def main(args: Array[String]): Unit ={
     if(args.length<2){
-      System.err.println("Usage of Parameters: master dataPath")
+      System.err.println("Usage of Parameters: master inputPath outputPath")
       System.exit(1)
     }
     val sparkConf = new SparkConf()
@@ -20,7 +23,7 @@ object TianChi {
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sc)
-    val user = sc.textFile(args(2)).map(_.split(","))
+    val user = sc.textFile(args(1)).map(_.split(","))
       .filter(line => line.length == 5)
       .map(a => User(a(0),a(1),a(2),a(3),a(4)))
     val userSchema = sqlContext.createDataFrame(user)
@@ -38,6 +41,12 @@ object TianChi {
     //val sqlString2 = "SELECT * FROM user"
     val result = sqlContext.sql(sqlString)
     result.collect().foreach(println)
+    val writer = new PrintWriter(new File(args(2)))
+    for(a <- result.collect()){
+      writer.write(a.toString())
+      writer.println()
+    }
+    writer.close()
 
   }
 
