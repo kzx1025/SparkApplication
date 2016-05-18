6df75f2667ed4197357457b5d7d19b11ef70aee5
iceke <iceke1025@gmail.com>
2016-05-18 16:46:28 +0800

    modified lr

--- build.sbt
+++ build.sbt
@@ -4,12 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.10.4"
 
-//libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
 
-//libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.4.1"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.6.1"
 
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
 
-//libraryDependencies += "org.apache.spark" % "spark-mllib_2.10"  % "1.4.1"
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.10"  % "1.6.1"
 
-//libraryDependencies += "org.apache.spark" %% "spark-mllib_2.10" % "1.4.1"
+//libraryDependencies += "org.apache.spark" %% "spark-mllib_2.10" % "1.6.1"
--- src/main/scala/tianchi/TianchiLR.scala
+++ src/main/scala/tianchi/TianchiLR.scala
@@ -1,10 +1,13 @@
 package tianchi
 
-import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.classification.{LogisticRegressionWithSGD, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.feature.StandardScaler
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.optimization.L1Updater
+import org.apache.spark.mllib.regression.{GeneralizedLinearModel, LinearRegressionWithSGD, LabeledPoint}
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 
 
@@ -14,15 +17,18 @@ import org.apache.spark.{SparkContext, SparkConf}
 object TianchiLR {
 
   def main(args: Array[String]): Unit = {
+    if (args.length < 3) {
+      System.err.println("Usage of Parameters: master positiveData negativeData model(1:LBFGS,2:SGD,3:DecisionTree) outputPath")
+      System.exit(1)
+    }
     val sparkConf = new SparkConf()
-      .setMaster("local")
+      .setMaster(args(0))
       .setAppName("TianChi")
-      //.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    //.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(sparkConf)
-    val data1 = sc.textFile("/Users/iceke/lab/TianChi/result_positive.csv")
-    val data2 = sc.textFile("/Users/iceke/lab/TianChi/result_negetive.csv")
-    val data3 = sc.textFile("/Users/iceke/lab/TianChi/traincsv.txt")
-    val data4 = sc.textFile("/Users/iceke/lab/TianChi/cvcsv.txt")
+    val data1 = sc.textFile(args(1))
+    val data2 = sc.textFile(args(2))
+
 
 
     val positiveData = data1.map { line =>
@@ -49,16 +55,17 @@ object TianchiLR {
     //标准正规化处理
     val scaler = new StandardScaler(withMean = true, withStd = true)
     val scaler2 = scaler.fit(allData.map(x => x.features))
+
     val finalData = allData.map(x => LabeledPoint(x.label, scaler2.transform(x.features)))
 
     val finalPositiveData = sc.parallelize(finalData.take(positiveDataNum.toInt))
     val finalNegativeData = sc.parallelize(finalData.collect().drop(positiveDataNum.toInt))
-    println(finalPositiveData.count()+","+finalNegativeData.count())
+    println(finalPositiveData.count() + "," + finalNegativeData.count())
 
 
     //负样本采样
-    val samplePositiveData = sc.parallelize(finalPositiveData.takeSample(withReplacement = false,positiveDataNum.toInt,42))
-    val sampleNegativeData = sc.parallelize(finalNegativeData.takeSample(withReplacement = false,positiveDataNum.toInt,42))
+    val samplePositiveData = sc.parallelize(finalPositiveData.takeSample(withReplacement = false, positiveDataNum.toInt, 42))
+    val sampleNegativeData = sc.parallelize(finalNegativeData.takeSample(withReplacement = false, positiveDataNum.toInt, 42))
 
 
 
@@ -69,20 +76,64 @@ object TianchiLR {
     val testData = positiveSplits(1) union negativeSplits(1)
 
 
-    val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(trainingData)
+    val choice = args(3).toInt
 
-    val labelAndPreds = testData.map { point =>
+    val labelAndPreds = {
+      if (choice == 1) {
+        //逻辑回归 最大似然估计
+        val model = new LogisticRegressionWithLBFGS().setNumClasses(2).run(trainingData)
+        testData.map { point =>
 
-      val prediction = model.predict(point.features)
+          val prediction = model.predict(point.features)
 
-      (point.label, prediction)
+          (point.label, prediction)
 
+        }
+
+      } else if (choice == 2) {
+        //逻辑回归 梯度下降法
+        val numIterations = 20000
+        val stepSize = 0.00000001
+        val lrWithSGD = new LogisticRegressionWithSGD()
+        lrWithSGD.optimizer.setNumIterations(numIterations)
+          .setStepSize(stepSize).setUpdater(new L1Updater())
+        val model = lrWithSGD.run(trainingData)
+
+        testData.map { point =>
+
+          val prediction = model.predict(point.features)
+
+          (point.label, prediction)
+
+        }
+
+      } else if (choice == 3) {
+        val numClasses = 2
+        val categoricalFeaturesInfo = Map[Int, Int]()
+        val impurity = "gini"
+        val maxDepth = 5
+        val maxBins = 32
+
+        //决策树
+        val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+          impurity, maxDepth, maxBins)
+        testData.map { point =>
+
+          val prediction = model.predict(point.features)
+
+          (point.label, prediction)
+
+        }
+
+
+      }
     }
 
-    val metrics = new MulticlassMetrics(labelAndPreds)
+
+    val metrics = new MulticlassMetrics(labelAndPreds.asInstanceOf[RDD[(Double,Double)]])
     val precision = metrics.precision
-    val trainErr = labelAndPreds.filter(r=> r._1 != r._2).count.toDouble / testData.count
-    println(trainErr+","+precision)
+    val trainErr = labelAndPreds.asInstanceOf[RDD[(Double,Double)]].filter(r => r._1 != r._2).count.toDouble / testData.count
+    println(trainErr + "," + precision)
 
 
   }
