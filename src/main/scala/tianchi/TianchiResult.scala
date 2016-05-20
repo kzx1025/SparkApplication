9d32b8806ea142e4cfc0d7aa4fd57e58cbb4ef7c
iceke <iceke1025@gmail.com>
2016-05-20 10:26:27 +0800

    fix

--- src/main/scala/tianchi/TianchiResult.scala
+++ src/main/scala/tianchi/TianchiResult.scala
@@ -91,10 +91,11 @@ object TianchiResult {
 
 
     //正负样本采样
+    val positiveDataNum = finalPositiveData.count()
    // val samplePositiveData = sc.parallelize(finalPositiveData.takeSample(withReplacement = false, positiveDataNum.toInt, 42))
     val samplePositiveData = finalPositiveData
-    val sampleNegativeData = finalNegativeData.sample(withReplacement = false,fraction*5,42L)
-    //val sampleNegativeData = sc.parallelize(finalNegativeData.takeSample(withReplacement = false, positiveDataNum.toInt, 42))
+   // val sampleNegativeData = finalNegativeData.sample(withReplacement = false,fraction*5,42L)
+    val sampleNegativeData = sc.parallelize(finalNegativeData.takeSample(withReplacement = false, positiveDataNum.toInt, 42))
 
 
 
