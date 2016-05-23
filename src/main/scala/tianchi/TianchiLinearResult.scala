799dbdddd3a3aa547ee4eec92ce53f5f95828c25
iceke <iceke1025@gmail.com>
2016-05-24 00:42:35 +0800

    fix

--- src/main/scala/tianchi/TianchiLinearResult.scala
+++ src/main/scala/tianchi/TianchiLinearResult.scala
@@ -2,23 +2,21 @@ package tianchi
 
 import java.io.{File, PrintWriter}
 
-import org.apache.spark.mllib.classification.{LogisticRegressionWithSGD, LogisticRegressionWithLBFGS}
 import org.apache.spark.mllib.feature.StandardScaler
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.optimization.L1Updater
-import org.apache.spark.mllib.regression._
+import org.apache.spark.mllib.regression.{RidgeRegressionWithSGD, LassoWithSGD, LinearRegressionWithSGD, LabeledPoint}
 import org.apache.spark.mllib.tree.configuration.BoostingStrategy
 import org.apache.spark.mllib.tree.{GradientBoostedTrees, RandomForest, DecisionTree}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by iceke on 16/5/23.
+  * Created by iceke on 16/5/24.
   */
-object TianchiLinear {
+object TianchiLinearResult {
   def main(args: Array[String]): Unit = {
     if (args.length < 5) {
-      System.err.println("Usage of Parameters: master positiveData1 positve testData " +
+      System.err.println("Usage of Parameters: master positiveData1  positveData2 testData " +
         "model(1:LBFGS,2:SGD,3:DecisionTree) outputPath ")
       System.exit(1)
     }
@@ -28,11 +26,11 @@ object TianchiLinear {
     //.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(sparkConf)
     val data1 = sc.textFile(args(1))
-    // val data2 = sc.textFile(args(2))
-    val data2 = null
+     val data2 = sc.textFile(args(2))
+     // val data2 = null
 
 
-    val dataA = data1
+    val dataA = data1 union data2
 
     val data5 = sc.textFile(args(3))
 
@@ -40,7 +38,7 @@ object TianchiLinear {
     val rawTestData = data5.map { line =>
       val parts = line.split(",").drop(2).map(_.toDouble)
 
-      LabeledPoint(parts(0), Vectors.dense(parts.slice(1, 6)))
+      LabeledPoint(parts(0), Vectors.dense(parts.slice(0, parts.length)))
     }
 
 
@@ -54,7 +52,7 @@ object TianchiLinear {
 
       val parts = line.split(",").drop(2).map(_.toDouble)
 
-      LabeledPoint(parts(0), Vectors.dense(parts.slice(1, 6)))
+      LabeledPoint(parts(0), Vectors.dense(parts.slice(1, parts.length)))
 
     }
 
@@ -97,7 +95,7 @@ object TianchiLinear {
 
           val prediction = model.predict(point.features)
 
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -112,7 +110,7 @@ object TianchiLinear {
 
           val prediction = model.predict(point.features)
 
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -125,7 +123,7 @@ object TianchiLinear {
         finalTestData.map { point =>
 
           val prediction = model.predict(point.features)
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -144,7 +142,7 @@ object TianchiLinear {
         finalTestData.map { point =>
 
           val prediction = model.predict(point.features)
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -162,7 +160,7 @@ object TianchiLinear {
         finalTestData.map { point =>
 
           val prediction = model.predict(point.features)
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -179,7 +177,7 @@ object TianchiLinear {
         finalTestData.map { point =>
 
           val prediction = model.predict(point.features)
-          (prediction, point.label)
+          prediction
 
         }.zip(artistInfo)
 
@@ -189,34 +187,21 @@ object TianchiLinear {
 
 
 
+    val evaluateData = resultData.asInstanceOf[RDD[(Double, (String, String))]]
+      .map(t => t._2._2+","+ t._2._1+","+t._1 )
+
+    val writer = new PrintWriter(new File(args(3)))
+    for(record <- evaluateData.collect()){
+      writer.write(record)
+      writer.println()
+    }
+
+    writer.close()
 
 
-    val evaluateData = resultData.asInstanceOf[RDD[((Double, Double), (String, String))]]
-      .map(t => ((t._2._2, t._2._1), (t._1._2, t._1._1)))
 
-    evaluateData.saveAsTextFile(args(5))
 
-    val days = evaluateData.map { t => (t._1._2, 1) }.reduceByKey(_ + _).count()
-
-    //(歌手,日期,实际播放数,预测值)
-    val tempValue = evaluateData.map { t => (t._1._1, Math.pow((t._2._2 - t._2._1) / t._2._1, 2.0)) }.reduceByKey(_ + _)
-    val tempValue2 = evaluateData.map { t => (t._1._1, Math.pow((t._2._2 - t._2._1) / t._2._1, 2.0)) }
-    tempValue2.collect().foreach(println)
-
-    //(歌手 方差)
-    val fangcha = tempValue.map(t => (t._1, Math.sqrt(t._2 / days)))
-    fangcha.collect().foreach(println)
-
-    //(歌手 权重)
-    val artistWeight = evaluateData.map { t => (t._1._1, t._2._1) }.reduceByKey(_ + _).map(t => (t._1, Math.sqrt(t._2)))
-
-    artistWeight.collect().foreach(println)
-    val scores = artistWeight.zip(fangcha).filter(t => t._1._1 == t._2._1).map(t => (t._1._1, t._1._2 * (1.0 - t._2._2)))
-      .map(t => (1, t._2)).reduceByKey(_ + _)
-
-    scores.collect().foreach(println)
 
   }
-
 
 }
