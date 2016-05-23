82d9d1b55b76689a4893cd1c7c94e69b1345434b
iceke <iceke1025@gmail.com>
2016-05-23 17:15:34 +0800

    fix

--- src/main/scala/tianchi/TianchiLinear.scala
+++ src/main/scala/tianchi/TianchiLinear.scala
@@ -17,7 +17,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 object TianchiLinear {
   def main(args: Array[String]): Unit = {
     if (args.length < 5) {
-      System.err.println("Usage of Parameters: master positiveData1  testData " +
+      System.err.println("Usage of Parameters: master positiveData1 positve testData " +
         "model(1:LBFGS,2:SGD,3:DecisionTree) outputPath ")
       System.exit(1)
     }
