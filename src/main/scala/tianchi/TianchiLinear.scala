b18f1a1289953472e9751f20a728edb23a01baaf
iceke <iceke1025@gmail.com>
2016-05-25 17:02:26 +0800

    fix

--- src/main/scala/tianchi/TianchiLinear.scala
+++ src/main/scala/tianchi/TianchiLinear.scala
@@ -47,7 +47,7 @@ object TianchiLinear {
       }
       println(add.length)
 
-      val features:Array[Double] = parts.slice(1, parts.length-args(5).toInt)++add.toArray
+      val features:Array[Double] = parts.slice(1, parts.length-args(5).toInt)++add
 
       LabeledPoint(parts(0), Vectors.dense(features))
       //LabeledPoint(parts(0), Vectors.dense(parts.slice(1,args(5).toInt-1)++parts.slice(args(5).toInt+1,parts.length)))
@@ -72,7 +72,7 @@ object TianchiLinear {
       }
       println(add.length)
 
-      val features:Array[Double] = parts.slice(1, parts.length-args(5).toInt)++add.toArray
+      val features:Array[Double] = parts.slice(1, parts.length-args(5).toInt)++add
 
       LabeledPoint(parts(0), Vectors.dense(features))
      // LabeledPoint(parts(0), Vectors.dense(parts.slice(1,args(5).toInt-1)++parts.slice(args(5).toInt+1,parts.length)))
